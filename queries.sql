b8be2b554949e75cd79eb2de3213917fab87efbe
Facundo Barneche <fh.barneche@gmail.com>
2024-11-19 21:31:38 -0300

    [IMPROVEMENT] Se agregan las queries del punto 3 a, b y c

--- queries.sql
+++ queries.sql
@@ -1,11 +1,6 @@
 -- a) Crear un reporte que muestre, por cada localidad, el hobby
 -- predominante y la cantidad de alumnos que lo practican.
 
--- Helpers
-SELECT * FROM LOCALIDAD order by id_localidad ASC;
-SELECT * FROM HOBBY ORDER BY id_hobby ASC;
-SELECT * FROM HOBBY_INTEGRANTE where id_integrante in (1,3,5,20,22,28,34);
-
 -- Esta query no obtiene el resultado esperado
 SELECT l.nombre AS Localidad, h.nombre AS Hobby_mas_comun, COUNT(hi.id_hobby) AS Total_alumnos FROM HOBBY_INTEGRANTE hi
 JOIN INTEGRANTE i ON (hi.id_integrante = i.id_integrante)
@@ -26,5 +21,25 @@ SELECT Localidad, Hobby_mas_comun, Total_alumnos FROM (
     JOIN HOBBY h ON hi.id_hobby = h.id_hobby
     JOIN LOCALIDAD l ON i.id_localidad = l.id_localidad
     GROUP BY l.nombre, h.nombre
-) sub
-WHERE rn = 1;
+) sub WHERE rn = 1;
+
+
+
+
+
+-- c) Identificar la materia más popular en cada grupo de estudio y el
+-- porcentaje de alumnos de cada grupo que está inscrito en esas materias.
+
+-- * 20 por regla de 3.... cantidad x 100% / 5 (max grupo)
+SELECT 
+m.nombre, 
+g.nombre, 
+COUNT(i.id_integrante) as total_alumno, 
+CONCAT(COUNT(i.id_integrante) * 20, '%') as porcentaje_grupo,
+ROW_NUMBER() OVER (PARTITION BY g.nombre ORDER BY COUNT(i.id_integrante) DESC) AS rn
+FROM ESTUDIANTE_GRUPO_ROL egr
+JOIN ESTUDIANTE i ON (egr.id_estudiante = i.id_integrante)
+JOIN GRUPO g ON (egr.id_grupo = g.id_grupo)
+JOIN ESTUDIANTE_MATERIA em ON (i.id_integrante = em.id_estudiante)
+JOIN MATERIA m ON (em.id_materia = m.id_materia)
+GROUP BY m.nombre, g.nombre;
